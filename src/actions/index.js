4a293e13adbe4b52955fed5bde6c3d6a0e3e0189
Luke Olali <mugapedia@gmail.com>
2018-12-03 09:29:00 +0300

    Creates action type and action creator for requesting posts for subreddit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
+export const REQUEST_POSTS = 'REQUEST_POSTS';
 
 export const selectSubreddit = subreddit => ({
   type: SELECT_SUBREDDIT,
@@ -8,5 +9,10 @@ export const selectSubreddit = subreddit => ({
 
 export const invalidateSubreddit = subreddit => ({
   type: INVALIDATE_SUBREDDIT,
+  subreddit
+})
+
+export const requestPosts = subreddit => ({
+  type: REQUEST_POSTS,
   subreddit
 })
