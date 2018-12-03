afd3894d37e5851cb240e45d95cd7ad8aac73c53
Luke Olali <mugapedia@gmail.com>
2018-12-03 09:37:34 +0300

    Defines action type and action creator for receiving posts when request is successful

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
 export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
 export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 
 export const selectSubreddit = subreddit => ({
   type: SELECT_SUBREDDIT,
@@ -15,4 +16,11 @@ export const invalidateSubreddit = subreddit => ({
 export const requestPosts = subreddit => ({
   type: REQUEST_POSTS,
   subreddit
+})
+
+export const receivePosts = (subreddit, json) => ({
+  type: RECEIVE_POSTS,
+  subreddit,
+  posts: json.data.children.map(child => child.data),
+  receivedAt: Date.now()
 })
