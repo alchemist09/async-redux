f455a5a56b39ee78ce6612190b112786dec2cc40
Luke Olali <mugapedia@gmail.com>
2018-12-03 08:41:25 +0300

    Adds action type and action creator for invalidating a subreddit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
 
 export const selectSubreddit = subreddit => ({
   type: SELECT_SUBREDDIT,
+  subreddit
+})
+
+export const invalidateSubreddit = subreddit => ({
+  type: INVALIDATE_SUBREDDIT,
   subreddit
 })
