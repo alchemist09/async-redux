bf071174f115d6c286d9ac5c44e453666b9b2b85
Luke Olali <mugapedia@gmail.com>
2018-12-05 12:58:13 +0300

    replaces the fetchPosts() method call with fetchPostsIfNeeded

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
-import { selectSubreddit, fetchPosts } from './actions';
+import { selectSubreddit, fetchPostsIfNeeded } from './actions';
 import rootReducer from './reducers';
 import './assets/css/index.css'
 import App from './components/App';
@@ -20,7 +20,7 @@ const store = createStore(
 );
 
 store.dispatch(selectSubreddit('reactjs'));
-store.dispatch(fetchPosts('reactjs')).then(() => console.log(store.getState()));
+store.dispatch(fetchPostsIfNeeded('reactjs')).then(() => console.log(store.getState()));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
