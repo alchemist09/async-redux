574088e95c757105c0c7c03b32f61929c7353793
Luke Olali <mugapedia@gmail.com>
2019-12-11 15:20:33 +0300

    Creates Posts components used to render posts from a subreddit

---
+++ src/components/Posts.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Posts extends Component {
+  render() {
+    return (
+      <ul>
+        {
+          this.props.posts.map((post, index) => (
+            <li key={index}>{post.title}</li>
+          ))
+        }
+      </ul>
+    )
+  }
+}
+
+Posts.propTyoes = {
+  posts: PropTypes.arrayOf.isRequired
+}
