9c360d0f12729c4159589e768d65152c7775405a
Luke Olali <mugapedia@gmail.com>
2019-12-11 15:47:43 +0300

    Connects AsyncApp component to the redux store and maps state values to component props

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class AsyncApp extends Component {
   
   render() {
+    const { selectedSubreddit, posts, isFetching, didInvalidate } = this.props;
     return (
       <div>
         
@@ -12,5 +14,16 @@ class AsyncApp extends Component {
   }
 }
 
-export default AsyncApp;
+const mapStateToProps = (state) => {
+  const { selectedSubreddit, postsBySubreddit } = state;
+  const { isFetching, didInvalidate, items: posts } = postsBySubreddit[selectedSubreddit] || { isFetching: true, items: [] }
 
+  return {
+    selectedSubreddit,
+    posts,
+    isFetching,
+    didInvalidate
+  }
+}
+
+export default connect(mapStateToProps)(AsyncApp);
