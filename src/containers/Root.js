d16c0b7e23905c84d4db711542ccaae505cb7a3f
Luke Olali <mugapedia@gmail.com>
2019-12-11 13:29:30 +0300

    Creates the Root container component and passes the redux store to it

---
+++ src/containers/Root.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Provider from 'react-redux';
+import configureSore from '../configureStore';
+import AsyncApp from './AsyncApp';
+
+const store = configureSore();
+
+export default class Root extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AsyncApp />
+      </Provider>
+    )
+  }
+}
