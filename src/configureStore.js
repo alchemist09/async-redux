96eeb68cf650e490578d3bebb319c0092778a085
Luke Olali <mugapedia@gmail.com>
2020-01-06 15:40:07 +0300

    Fixes typo on applyMiddleware function imported from redux

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, appleMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddlware  from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
@@ -9,7 +9,7 @@ const configureStore = initialState => (
   createStore(
     rootReducer, 
     initialState,
-    appleMiddleware(thunkMiddlware, loggerMiddleware)
+    applyMiddleware(thunkMiddlware, loggerMiddleware)
   ) 
 );
 
